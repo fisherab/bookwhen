63f8d8d4777922d44e3be3c83ce9fc0ea1253964
Steve Fisher <dr.s.m.fisher@gmail.com>
2020-12-17 14:36:33 +0000

    Produce csv output

--- rbp.py
+++ rbp.py
@@ -2,16 +2,18 @@
 import requests
 from requests.auth import HTTPBasicAuth
 import json
-from datetime import datetime
+from datetime import datetime, date
 with open("key.txt","r") as f:
     key = f.readline().strip()
 baseurl = "https://api.bookwhen.com/v2/"
 auth = HTTPBasicAuth(key ,"")
-response = requests.get(baseurl+"events?filter[tag]=RealBridge&filter[from]=20200101",auth=auth)
+response = requests.get(baseurl+"events?filter[tag]=RealBridge&filter[from]=20200101&filter[to]="+date.today().strftime("%Y%m%d"),auth=auth)
 output = json.loads(response.content)
-for datum in output['data']:
-    attributes = datum['attributes']
-    title=attributes['title']
-    start_at = datetime.fromisoformat(attributes['start_at']).date()
-    attendee_count = attributes['attendee_count']
-    print(start_at, title, "has", attendee_count, "tickets issued")
+with open("bw.csv","w") as ofile:
+    print('Date','Name','Tickets', sep=",", file=ofile)
+    for datum in output['data']:
+        attributes = datum['attributes']
+        title=attributes['title']
+        start_at = datetime.fromisoformat(attributes['start_at']).date()
+        attendee_count = attributes['attendee_count']
+        print(start_at, title, attendee_count, sep=",", file=ofile)
