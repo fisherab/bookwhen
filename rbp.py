06a292fd6fc671bfe678039cffcb37df80907733
Steve Fisher <dr.s.m.fisher@gmail.com>
2021-03-11 00:46:10 +0000

    Do it for the last month

--- rbp.py
+++ rbp.py
@@ -2,18 +2,30 @@
 import requests
 from requests.auth import HTTPBasicAuth
 import json
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 with open("key.txt","r") as f:
     key = f.readline().strip()
 baseurl = "https://api.bookwhen.com/v2/"
+today = date.today()
+firstOfMonth = today.replace(day=1)
+lastOfPreviousMonth = firstOfMonth - timedelta(days=1)
+firstOfPreviousMonth = lastOfPreviousMonth.replace(day=1)
+dfrom = firstOfPreviousMonth.strftime("%Y%m%d")
+dto = lastOfPreviousMonth.strftime("%Y%m%d")
+
+url = baseurl+"events?filter[tag]=RealBridge&filter[from]="+dfrom+"&filter[to]="+dto
 auth = HTTPBasicAuth(key ,"")
-response = requests.get(baseurl+"events?filter[tag]=RealBridge&filter[from]=20200101&filter[to]="+date.today().strftime("%Y%m%d"),auth=auth)
-output = json.loads(response.content)
 with open("bw.csv","w") as ofile:
     print('Date','Name','Tickets', sep=",", file=ofile)
-    for datum in output['data']:
-        attributes = datum['attributes']
-        title=attributes['title']
-        start_at = datetime.fromisoformat(attributes['start_at']).date()
-        attendee_count = attributes['attendee_count']
-        print(start_at, title, attendee_count, sep=",", file=ofile)
+    while True:
+        response = requests.get(url,auth=auth)
+        output = json.loads(response.content)
+        for datum in output['data']:
+            attributes = datum['attributes']
+            title=attributes['title']
+            start_at = datetime.fromisoformat(attributes['start_at']).date()
+            attendee_count = attributes['attendee_count']
+            print(start_at, title, attendee_count, sep=",", file=ofile)
+        links = output['links']
+        if 'next' not in links: break
+        url = links['next']
